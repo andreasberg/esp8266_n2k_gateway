867d7b73cec29142e072d5b5a4dca68300013e18
Andreas Berg <Andreas.Berg@aberea.com>
2017-06-09 11:29:18 +0300

    Removed tools

--- tools/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm LINK_TO_LOCAL_ARDUINO_LIBRARIES
-rm LINK_TO_ESP8266
-ln -s ~/Documents/Arduino/libraries/ LINK_TO_LOCAL_ARDUINO_LIBRARIES
-ln -s /Applications/Arduino.app/Contents/Java/hardware/esp8266com LINK_TO_ESP8266
